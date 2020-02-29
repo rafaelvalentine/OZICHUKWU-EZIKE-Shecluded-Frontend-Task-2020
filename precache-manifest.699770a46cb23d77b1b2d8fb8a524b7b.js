f8993fa8f7bab939b729ffc84fba2ddfa7e9c989
rafaelvalentine <ozichukwuezike@hotmail.com>
2020-02-29 14:20:04 +0100

    Updates

---
+++ precache-manifest.699770a46cb23d77b1b2d8fb8a524b7b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33cc873e99dee678442c76acc99aba32",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/index.html"
+  },
+  {
+    "revision": "c21d6cf040c48e3d9bfc",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e21c762db08bb837e1ce",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/css/main.e9d279e0.chunk.css"
+  },
+  {
+    "revision": "c21d6cf040c48e3d9bfc",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/js/2.d3569894.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/js/2.d3569894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21c762db08bb837e1ce",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/js/main.e10475d9.chunk.js"
+  },
+  {
+    "revision": "848267fc6ec070328178",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/js/runtime-main.c6497cf4.js"
+  },
+  {
+    "revision": "d0ea87eaf9bbb8343e63efd63310c254",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/media/arrow-drop-left.d0ea87ea.svg"
+  },
+  {
+    "revision": "758ebe3ef18d8e8ef9782fc2ce8e80f8",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/media/arrow-drop-right.758ebe3e.svg"
+  },
+  {
+    "revision": "f1b9524584ce8f45d8198321b28c60b3",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/media/arrow-dropdown.f1b95245.svg"
+  }
+]);
