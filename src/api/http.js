9374901613a3df6efb3d5b4528078c0df916ab47
rafaelvalentine <ozichukwuezike@hotmail.com>
2020-02-29 14:02:20 +0100

    added .env file

--- src/api/http.js
+++ src/api/http.js
@@ -2,13 +2,13 @@ import axios from "axios";
 const catApi = () => axios.create({
     baseURL: 'https://api.thecatapi.com/v1/images/search',
     headers: {
-        'x-api-key': `5cb17320-581b-4472-87e5-a2cd0d3d8870`
+        'x-api-key': process.env.REACT_APP_catAPI_Key
     }
 });
 const lotrApi = () => axios.create({
     baseURL: 'https://the-one-api.herokuapp.com/v1',
     headers: {
-        Authorization: `Bearer XuyMN15-Bh5C34T1oiQy`
+        Authorization: `Bearer ${process.env.REACT_APP_lotrAPI_Key}`
     }
 });
 export default () => ({
