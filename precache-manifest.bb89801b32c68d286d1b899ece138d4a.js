f781d7b82497bed052ce47221c7d307d31a40fdf
rafaelvalentine <ozichukwuezike@hotmail.com>
2020-02-29 15:16:31 +0100

    Updates

---
+++ precache-manifest.bb89801b32c68d286d1b899ece138d4a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fabe9b809d6bee9854d75f1c390226",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/index.html"
+  },
+  {
+    "revision": "1ee6de62510964bdd701",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e76b7d912ccdbc783682",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/css/main.871efcd5.chunk.css"
+  },
+  {
+    "revision": "1ee6de62510964bdd701",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/js/2.090d9753.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/js/2.090d9753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76b7d912ccdbc783682",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/js/main.472d114a.chunk.js"
+  },
+  {
+    "revision": "848267fc6ec070328178",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/js/runtime-main.c6497cf4.js"
+  },
+  {
+    "revision": "5f7eb326ebba2495acee0c795276b7a5",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/media/argonath.5f7eb326.jpg"
+  },
+  {
+    "revision": "d0ea87eaf9bbb8343e63efd63310c254",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/media/arrow-drop-left.d0ea87ea.svg"
+  },
+  {
+    "revision": "758ebe3ef18d8e8ef9782fc2ce8e80f8",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/media/arrow-drop-right.758ebe3e.svg"
+  },
+  {
+    "revision": "f1b9524584ce8f45d8198321b28c60b3",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/media/arrow-dropdown.f1b95245.svg"
+  },
+  {
+    "revision": "3de5c276694c65cf610831d843cfd274",
+    "url": "/OZICHUKWU-EZIKE-Shecluded-Frontend-Task-2020/static/media/the_one_ring.3de5c276.jpg"
+  }
+]);
